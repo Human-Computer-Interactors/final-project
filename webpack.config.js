1368766975434085bbc7ece4ac1aa7e0007d096d
Emily Sturman <emily@sturman.org>
2023-04-13 12:38:46 -0500

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,6 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
-  // config.resolve.alias["react-native-track-player"] = "react-native-track-player-web";
+  config.resolve.alias["react-native-track-player"] = "react-native-track-player-web";
   return config;
 };
