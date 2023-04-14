1a424d68e402a860a7ce7cc9530f7290a5f40c4b
Manish Bhandari <bhmanish73@gmail.com>
2023-04-14 04:13:41 -0400

    mixes screen

--- components/ClipsViewer.tsx
+++ components/ClipsViewer.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useRef, useState, useMemo } from "react";
 import {
-  View,
-  StyleSheet,
-  FlatList,
-  useWindowDimensions,
-  Animated,
-  Easing
+	View,
+	StyleSheet,
+	FlatList,
+	useWindowDimensions,
+	Animated,
+	Easing,
 } from "react-native";
 import { useAppSelector } from "../redux/hooks";
 import type { FunctionComponent } from "react";
@@ -13,199 +13,229 @@ import { Colors } from "../types/Colors";
 import { range, prefixSums } from "../utilities";
 
 type ClipsViewerProps = {
-  currentTrack: number,
-  selectedMode: boolean,
-  playing: boolean,
-  playTimer: NodeJS.Timeout | null,
-  mixId: string,
-  pause: () => void,
-  skipTo: (position: number, relative: boolean, timeOffset: number, startPlaying: boolean) => void,
+	currentTrack: number;
+	selectedMode: boolean;
+	playing: boolean;
+	playTimer: NodeJS.Timeout | null;
+	mixId: string;
+	pause: () => void;
+	skipTo: (
+		position: number,
+		relative: boolean,
+		timeOffset: number,
+		startPlaying: boolean
+	) => void;
 };
 
 const ONE_SECOND_WIDTH = 5;
 
-const ClipsViewer: FunctionComponent<ClipsViewerProps> = ({ currentTrack, selectedMode, playing, playTimer, mixId, pause, skipTo }) => {
-  const scrollRef = useRef<FlatList>(null);
-  const scrollAnimation = useRef<Animated.Value>(new Animated.Value(0));
-  const [contentWidth, setContentWidth] = useState<number>(0);
-  const [scrollOffset, setScrollOffset] = useState<number>(0);
-  const [wasPlaying, setWasPlaying] = useState<boolean>(false);
-  const [isDrag, setIsDrag] = useState<boolean>(false);
-  const [scrollTimer, setScrollTimer] = useState<NodeJS.Timeout | null>(null);
+const ClipsViewer: FunctionComponent<ClipsViewerProps> = ({
+	currentTrack,
+	selectedMode,
+	playing,
+	playTimer,
+	mixId,
+	pause,
+	skipTo,
+}) => {
+	const scrollRef = useRef<FlatList>(null);
+	const scrollAnimation = useRef<Animated.Value>(new Animated.Value(0));
+	const [contentWidth, setContentWidth] = useState<number>(0);
+	const [scrollOffset, setScrollOffset] = useState<number>(0);
+	const [wasPlaying, setWasPlaying] = useState<boolean>(false);
+	const [isDrag, setIsDrag] = useState<boolean>(false);
+	const [scrollTimer, setScrollTimer] = useState<NodeJS.Timeout | null>(null);
 
-  const trackSegments = useAppSelector(({ mixes }) => mixes[mixId].tracks);
-  const tracks = useAppSelector(({ tracks }) => tracks);
-  const { width } = useWindowDimensions();
+	const trackSegments = useAppSelector(({ mixes }) => mixes[mixId].tracks);
+	const tracks = useAppSelector(({ tracks }) => tracks);
+	const { width } = useWindowDimensions();
 
-  const trackOffsets = useMemo<number[]>(
-    () => prefixSums(trackSegments.map(({ start, end }) => (end - start) * ONE_SECOND_WIDTH)),
-    [trackSegments]
-  );
+	const trackOffsets = useMemo<number[]>(
+		() =>
+			prefixSums(
+				trackSegments.map(({ start, end }) => (end - start) * ONE_SECOND_WIDTH)
+			),
+		[trackSegments]
+	);
 
-  // Find the current track segment based on scroll position
-  const getScrollLocation = (slop: number = 0) => {
-    let index = trackOffsets.findIndex((pos, i) => (
-      pos <= scrollOffset + slop
-      && i + 1 < trackOffsets.length
-      && trackOffsets[i + 1] > scrollOffset + slop
-    ));
-    // Set to first or last segment if user over-scrolls
-    if (index === -1) {
-      index = scrollOffset < 0 ? 0 : trackSegments.length - 1;
-    }
-    return index;
-  }
+	// Find the current track segment based on scroll position
+	const getScrollLocation = (slop: number = 0) => {
+		let index = trackOffsets.findIndex(
+			(pos, i) =>
+				pos <= scrollOffset + slop &&
+				i + 1 < trackOffsets.length &&
+				trackOffsets[i + 1] > scrollOffset + slop
+		);
+		// Set to first or last segment if user over-scrolls
+		if (index === -1) {
+			index = scrollOffset < 0 ? 0 : trackSegments.length - 1;
+		}
+		return index;
+	};
 
-  const beginScrollInterrupt = () => {
-    scrollAnimation.current.stopAnimation();
-    setWasPlaying(playing);
-    setIsDrag(true);
-    pause();
-  };
+	const beginScrollInterrupt = () => {
+		scrollAnimation.current.stopAnimation();
+		setWasPlaying(playing);
+		setIsDrag(true);
+		pause();
+	};
 
-  const endScrollInterrupt = () => {
-    if (!isDrag) return;
-    const index = getScrollLocation();
-    // Find offset from start of segment in seconds based on scroll position
-    const timeOffset = Math.min(
-      Math.max(
-        (scrollOffset - trackOffsets[index]) / ONE_SECOND_WIDTH,
-        0 // Set to 0 if user over-scrolls to the left (on first track)
-      ), trackSegments[index].end // Set to end if user over-scrolls on right (on last track)
-    );
-    skipTo(index, false, timeOffset, wasPlaying);
-    setIsDrag(false);
-  };
+	const endScrollInterrupt = () => {
+		if (!isDrag) return;
+		const index = getScrollLocation();
+		// Find offset from start of segment in seconds based on scroll position
+		const timeOffset = Math.min(
+			Math.max(
+				(scrollOffset - trackOffsets[index]) / ONE_SECOND_WIDTH,
+				0 // Set to 0 if user over-scrolls to the left (on first track)
+			),
+			trackSegments[index].end // Set to end if user over-scrolls on right (on last track)
+		);
+		skipTo(index, false, timeOffset, wasPlaying);
+		setIsDrag(false);
+	};
 
-  const scrollEndDrag = () => {
-    const timerId = setTimeout(endScrollInterrupt, 100);
-    setScrollTimer(timerId);
-  };
+	const scrollEndDrag = () => {
+		const timerId = setTimeout(endScrollInterrupt, 100);
+		setScrollTimer(timerId);
+	};
 
-  const playingAnimation = (offset: number = scrollOffset) => (
-    Animated.sequence(range(
-      Math.floor((contentWidth - width - offset) / ONE_SECOND_WIDTH),
-      offset + ONE_SECOND_WIDTH,
-      ONE_SECOND_WIDTH
-    ).map((value) => Animated.timing(scrollAnimation.current, {
-      toValue: value,
-      duration: 1000,
-      useNativeDriver: true,
-      easing: Easing.linear
-    })))
-  );
+	const playingAnimation = (offset: number = scrollOffset) =>
+		Animated.sequence(
+			range(
+				Math.floor((contentWidth - width - offset) / ONE_SECOND_WIDTH),
+				offset + ONE_SECOND_WIDTH,
+				ONE_SECOND_WIDTH
+			).map((value) =>
+				Animated.timing(scrollAnimation.current, {
+					toValue: value,
+					duration: 1000,
+					useNativeDriver: true,
+					easing: Easing.linear,
+				})
+			)
+		);
 
-  const skippingAnimation = () => (
-    Animated.timing(scrollAnimation.current, {
-      toValue: trackOffsets[currentTrack],
-      duration: 300,
-      useNativeDriver: true,
-      easing: Easing.inOut(Easing.ease)
-    })
-  );
+	const skippingAnimation = () =>
+		Animated.timing(scrollAnimation.current, {
+			toValue: trackOffsets[currentTrack],
+			duration: 300,
+			useNativeDriver: true,
+			easing: Easing.inOut(Easing.ease),
+		});
 
-  const skip = () => {
-    const animations = [skippingAnimation()];
-    if (playing) {
-      scrollAnimation.current.stopAnimation();
-      animations.push(playingAnimation(trackOffsets[currentTrack]));
-    }
-    Animated.sequence(animations).start();
-  };
+	const skip = () => {
+		const animations = [skippingAnimation()];
+		if (playing) {
+			scrollAnimation.current.stopAnimation();
+			animations.push(playingAnimation(trackOffsets[currentTrack]));
+		}
+		Animated.sequence(animations).start();
+	};
 
-  useEffect(() => {
-    scrollAnimation.current.addListener((animation) => {
-      scrollRef.current?.scrollToOffset({
-        offset: animation.value,
-        animated: false
-      });
-    });
-    return () => scrollAnimation.current.removeAllListeners();
-  }, []);
+	useEffect(() => {
+		scrollAnimation.current.addListener((animation) => {
+			scrollRef.current?.scrollToOffset({
+				offset: animation.value,
+				animated: false,
+			});
+		});
+		return () => scrollAnimation.current.removeAllListeners();
+	}, []);
 
-  useEffect(() => {
-    if (contentWidth) {
-      if (playing) {
-        scrollAnimation.current.removeAllListeners();
-        scrollAnimation.current = new Animated.Value(scrollOffset);
-        scrollAnimation.current.addListener((animation) => {
-          scrollRef.current?.scrollToOffset({
-            offset: animation.value,
-            animated: false
-          });
-        });
-        playingAnimation().start();
-      } else {
-        scrollAnimation.current.stopAnimation();
-      }
-    }
-  }, [contentWidth, playing]);
+	useEffect(() => {
+		if (contentWidth) {
+			if (playing) {
+				scrollAnimation.current.removeAllListeners();
+				scrollAnimation.current = new Animated.Value(scrollOffset);
+				scrollAnimation.current.addListener((animation) => {
+					scrollRef.current?.scrollToOffset({
+						offset: animation.value,
+						animated: false,
+					});
+				});
+				playingAnimation().start();
+			} else {
+				scrollAnimation.current.stopAnimation();
+			}
+		}
+	}, [contentWidth, playing]);
 
-  useEffect(() => {
-    // Skip to different track
-    if (getScrollLocation() !== currentTrack) {
-      skip();
-    }
-  }, [currentTrack]);
+	useEffect(() => {
+		// Skip to different track
+		if (getScrollLocation() !== currentTrack) {
+			skip();
+		}
+	}, [currentTrack]);
 
-  useEffect(() => {
-    // Skip back to beginning if track is being looped
-    if (selectedMode && getScrollLocation(ONE_SECOND_WIDTH) > currentTrack) {
-      skip();
-    }
-  }, [playTimer]);
+	useEffect(() => {
+		// Skip back to beginning if track is being looped
+		if (selectedMode && getScrollLocation(ONE_SECOND_WIDTH) > currentTrack) {
+			skip();
+		}
+	}, [playTimer]);
 
-  return (
-    <View style={styles.container}>
-      <View style={[styles.pointer, { left: width / 2 }]} />
-      <Animated.FlatList
-        style={{ width }}
-        ref={scrollRef}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        onContentSizeChange={(width) => setContentWidth(width)}
-        onScrollBeginDrag={beginScrollInterrupt}
-        onScrollEndDrag={scrollEndDrag}
-        onMomentumScrollBegin={() => clearTimeout(scrollTimer)}
-        onMomentumScrollEnd={endScrollInterrupt}
-        onScroll={({ nativeEvent }) => setScrollOffset(nativeEvent.contentOffset.x)}
-        contentContainerStyle={styles.contentContainer}
-        data={trackSegments}
-        keyExtractor={(segment) => `${segment.id}-${segment.start}-${segment.end}`}
-        renderItem={({ item, index }) => (
-          <View style={[styles.trackSegment, {
-            width: ONE_SECOND_WIDTH * (item.end - item.start),
-            backgroundColor: !selectedMode || currentTrack === index
-              ? Colors[tracks[item.id].color]
-              : "#a4a4a4"
-          }]} />
-        )}
-      />
-    </View>
-  )
+	return (
+		<View style={styles.container}>
+			<View style={[styles.pointer, { left: width / 2 }]} />
+			<Animated.FlatList
+				style={{ width }}
+				ref={scrollRef}
+				horizontal
+				showsHorizontalScrollIndicator={false}
+				onContentSizeChange={(width) => setContentWidth(width)}
+				onScrollBeginDrag={beginScrollInterrupt}
+				onScrollEndDrag={scrollEndDrag}
+				onMomentumScrollBegin={() => clearTimeout(scrollTimer)}
+				onMomentumScrollEnd={endScrollInterrupt}
+				onScroll={({ nativeEvent }) =>
+					setScrollOffset(nativeEvent.contentOffset.x)
+				}
+				contentContainerStyle={styles.contentContainer}
+				data={trackSegments}
+				keyExtractor={(segment) =>
+					`${segment.id}-${segment.start}-${segment.end}`
+				}
+				renderItem={({ item, index }) => (
+					<View
+						style={[
+							styles.trackSegment,
+							{
+								width: ONE_SECOND_WIDTH * (item.end - item.start),
+								backgroundColor:
+									!selectedMode || currentTrack === index
+										? Colors[tracks[item.id].color]
+										: "#a4a4a4",
+							},
+						]}
+					/>
+				)}
+			/>
+		</View>
+	);
 };
 
 export default ClipsViewer;
 
 const styles = StyleSheet.create({
-  container: {
-    marginVertical: 10
-  },
-  pointer: {
-    position: "absolute",
-    height: "100%",
-    width: 2,
-    backgroundColor: "#000",
-    zIndex: 100
-  },
-  contentContainer: {
-    flexDirection: "row",
-    paddingHorizontal: "50%"
-  },
-  trackSegment: {
-    backgroundColor: "#fff",
-    height: 150,
-    marginVertical: 50,
-    borderWidth: 1
-  }
-})
+	container: {
+		marginVertical: 10,
+	},
+	pointer: {
+		position: "absolute",
+		height: "100%",
+		width: 2,
+		backgroundColor: "#000",
+		zIndex: 100,
+	},
+	contentContainer: {
+		flexDirection: "row",
+		paddingHorizontal: "50%",
+	},
+	trackSegment: {
+		backgroundColor: "#fff",
+		height: 150,
+		marginVertical: 50,
+		borderWidth: 1,
+	},
+});
--- pages/MixScreen.tsx
+++ pages/MixScreen.tsx
@@ -1,11 +1,11 @@
 import type { FunctionComponent } from "react";
 import { useEffect, useState } from "react";
 import {
-  StyleSheet,
-  Text,
-  useWindowDimensions,
-  View,
-  Platform
+	StyleSheet,
+	Text,
+	useWindowDimensions,
+	View,
+	Platform,
 } from "react-native";
 import type { DragEndParams } from "react-native-draggable-flatlist";
 import type { NavigatorProps } from "../navigation/StackNavigator";
@@ -22,244 +22,266 @@ import { range, modulo } from "../utilities";
 
 type MixScreenProps = NavigatorProps<"Mix">;
 
-const MixScreen: FunctionComponent<MixScreenProps> = ({ route, navigation }) => {
-  // Index of current track being played
-  const [currentTrack, setCurrentTrack] = useState<number>(0);
-  // Whether player is in "Selected Mode" (will repeat single track segment)
-  const [selectedMode, setSelectedMode] = useState<boolean>(false);
-  const [playing, setPlaying] = useState<boolean>(false);
-  const [playTimer, setPlayTimer] = useState<NodeJS.Timeout | null>(null);
-  const [playerHeight, setPlayerHeight] = useState<number>(0);
+const MixScreen: FunctionComponent<MixScreenProps> = ({
+	route,
+	navigation,
+}) => {
+	// Index of current track being played
+	const [currentTrack, setCurrentTrack] = useState<number>(0);
+	// Whether player is in "Selected Mode" (will repeat single track segment)
+	const [selectedMode, setSelectedMode] = useState<boolean>(false);
+	const [playing, setPlaying] = useState<boolean>(false);
+	const [playTimer, setPlayTimer] = useState<NodeJS.Timeout | null>(null);
+	const [playerHeight, setPlayerHeight] = useState<number>(0);
 
-  const { mixId } = route.params;
-  const mix = useAppSelector(({ mixes }) => mixes[mixId]);
-  const tracks = useAppSelector(({ tracks }) => tracks);
-  const dispatch = useAppDispatch();
-  const { top, bottom } = useSafeAreaInsets();
-  const { height, width } = useWindowDimensions();
+	const { mixId } = route.params;
+	const mix = useAppSelector(({ mixes }) => mixes[mixId]);
+	const tracks = useAppSelector(({ tracks }) => tracks);
+	const dispatch = useAppDispatch();
+	const { top, bottom } = useSafeAreaInsets();
+	const { height, width } = useWindowDimensions();
 
-  const skipTo = async (position: number, relative: boolean = true, timeOffset: number = 0, startPlaying: boolean = false) => {
-    let index = position;
-    if (relative) {
-      index = modulo(
-        await TrackPlayer.getCurrentTrack() + position,
-        mix.tracks.length
-      );
-    }
-    const mixTrack = mix.tracks[index];
-    const start = mixTrack.start + timeOffset;
-    await TrackPlayer.skip(index, start);
-    setCurrentTrack(index);
-    if (startPlaying) {
-      await TrackPlayer.play();
-      setPlaying(true);
-    }
-    if (playing || startPlaying) {
-      const timeoutId = setTimeout(
-        selectedMode ? repeat : skipForward,
-        (mixTrack.end - start) * 1000,
-        true
-      );
-      clearTimeout(playTimer);
-      setPlayTimer(timeoutId);
-    }
-  }
+	const skipTo = async (
+		position: number,
+		relative: boolean = true,
+		timeOffset: number = 0,
+		startPlaying: boolean = false
+	) => {
+		let index = position;
+		if (relative) {
+			index = modulo(
+				(await TrackPlayer.getCurrentTrack()) + position,
+				mix.tracks.length
+			);
+		}
+		const mixTrack = mix.tracks[index];
+		const start = mixTrack.start + timeOffset;
+		await TrackPlayer.skip(index, start);
+		setCurrentTrack(index);
+		if (startPlaying) {
+			await TrackPlayer.play();
+			setPlaying(true);
+		}
+		if (playing || startPlaying) {
+			const timeoutId = setTimeout(
+				selectedMode ? repeat : skipForward,
+				(mixTrack.end - start) * 1000,
+				true
+			);
+			clearTimeout(playTimer);
+			setPlayTimer(timeoutId);
+		}
+	};
 
-  const repeat = (startPlaying: boolean) => skipTo(0, true, 0, startPlaying);
-  const skipForward = () => skipTo(1);
-  const skipBackward = () => skipTo(-1);
+	const repeat = (startPlaying: boolean) => skipTo(0, true, 0, startPlaying);
+	const skipForward = () => skipTo(1);
+	const skipBackward = () => skipTo(-1);
 
-  const play = async () => {
-    const pos = await TrackPlayer.getPosition();
-    const track = await TrackPlayer.getCurrentTrack();
-    await TrackPlayer.play();
-    setPlaying(true);
-    const timerId = setTimeout(
-      selectedMode ? repeat : skipForward,
-      (mix.tracks[track].end - pos) * 1000,
-      true
-    );
-    setPlayTimer(timerId);
-  };
+	const play = async () => {
+		const pos = await TrackPlayer.getPosition();
+		const track = await TrackPlayer.getCurrentTrack();
+		await TrackPlayer.play();
+		setPlaying(true);
+		const timerId = setTimeout(
+			selectedMode ? repeat : skipForward,
+			(mix.tracks[track].end - pos) * 1000,
+			true
+		);
+		setPlayTimer(timerId);
+	};
 
-  const pause = async () => {
-    clearTimeout(playTimer);
-    await TrackPlayer.pause();
-    setPlaying(false);
-  };
+	const pause = async () => {
+		clearTimeout(playTimer);
+		await TrackPlayer.pause();
+		setPlaying(false);
+	};
 
-  const selectTrack = async (index: number) => {
-    if (index !== currentTrack) {
-      setCurrentTrack(index);
-      await skipTo(index, false);
-    }
-    setSelectedMode(true);
-  }
+	const selectTrack = async (index: number) => {
+		if (index !== currentTrack) {
+			setCurrentTrack(index);
+			await skipTo(index, false);
+		}
+		setSelectedMode(true);
+	};
 
-  const deselectTrack = async () => {
-    setSelectedMode(false);
-  };
+	const deselectTrack = async () => {
+		setSelectedMode(false);
+	};
 
-  const reorderQueue = async ({ data, from, to } : DragEndParams<TrackSegment>) => {
-    if (from === to) return;
-    // Special handling if moving the current track (to allow it to keep playing)
-    if (from === currentTrack) {
-      if (from > to) {
-        // Add moved tracks after track, then remove them from before track
-        await TrackPlayer.add(
-          mix.tracks.slice(to, from).map(({ id }) => tracks[id]),
-          from + 1
-        );
-        await TrackPlayer.remove(range(to, from));
-      } else {
-        // Remove tracks from after track, then add them to before track
-        await TrackPlayer.remove(range(from, to));
-        await TrackPlayer.add(
-          mix.tracks.slice(from + 1, to + 1).map(({ id }) => tracks[id]),
-          from
-        )
-      }
-    } else {
-      if (from > to) {
-        await TrackPlayer.remove(from);
-        await TrackPlayer.add(tracks[mix.tracks[from].id], to);
-      } else {
-        await TrackPlayer.add(tracks[mix.tracks[from].id], to);
-        await TrackPlayer.remove(from);
-      }
-    }
-    dispatch(reorderTracks({ mixId, tracks: data }));
-  };
+	const reorderQueue = async ({
+		data,
+		from,
+		to,
+	}: DragEndParams<TrackSegment>) => {
+		if (from === to) return;
+		// Special handling if moving the current track (to allow it to keep playing)
+		if (from === currentTrack) {
+			if (from > to) {
+				// Add moved tracks after track, then remove them from before track
+				await TrackPlayer.add(
+					mix.tracks.slice(to, from).map(({ id }) => tracks[id]),
+					from + 1
+				);
+				await TrackPlayer.remove(range(to, from));
+			} else {
+				// Remove tracks from after track, then add them to before track
+				await TrackPlayer.remove(range(from, to));
+				await TrackPlayer.add(
+					mix.tracks.slice(from + 1, to + 1).map(({ id }) => tracks[id]),
+					from
+				);
+			}
+		} else {
+			if (from > to) {
+				await TrackPlayer.remove(from);
+				await TrackPlayer.add(tracks[mix.tracks[from].id], to);
+			} else {
+				await TrackPlayer.add(tracks[mix.tracks[from].id], to);
+				await TrackPlayer.remove(from);
+			}
+		}
+		dispatch(reorderTracks({ mixId, tracks: data }));
+	};
 
-  // Set up queue on page load
-  useEffect(() => {
-    const setUpQueue = async () => {
-      await TrackPlayer.reset();
-      await TrackPlayer.add(mix.tracks.map(({ id }) => tracks[id]));
-      await TrackPlayer.skip(0);
-    }
-    setUpQueue();
-    return () => {
-      TrackPlayer.reset();
-    };
-  }, []);
+	// Set up queue on page load
+	useEffect(() => {
+		const setUpQueue = async () => {
+			await TrackPlayer.reset();
+			await TrackPlayer.add(mix.tracks.map(({ id }) => tracks[id]));
+			await TrackPlayer.skip(0);
+		};
+		setUpQueue();
+		return () => {
+			TrackPlayer.reset();
+		};
+	}, []);
 
-  // Adjust timer to repeat instead of skipping to next track in selected mode
-  useEffect(() => {
-    if (!playing) return;
-    const changeToFromRepeat = async (selectedMode: boolean) => {
-      const track = await TrackPlayer.getCurrentTrack();
-      const pos = await TrackPlayer.getPosition();
-      const timerId = setTimeout(
-        selectedMode ? repeat : skipForward,
-        (mix.tracks[track].end - pos) * 1000,
-        false
-      );
-      clearTimeout(playTimer);
-      setPlayTimer(timerId);
-    };
-    changeToFromRepeat(selectedMode);
-  }, [selectedMode]);
+	// Adjust timer to repeat instead of skipping to next track in selected mode
+	useEffect(() => {
+		if (!playing) return;
+		const changeToFromRepeat = async (selectedMode: boolean) => {
+			const track = await TrackPlayer.getCurrentTrack();
+			const pos = await TrackPlayer.getPosition();
+			const timerId = setTimeout(
+				selectedMode ? repeat : skipForward,
+				(mix.tracks[track].end - pos) * 1000,
+				false
+			);
+			clearTimeout(playTimer);
+			setPlayTimer(timerId);
+		};
+		changeToFromRepeat(selectedMode);
+	}, [selectedMode]);
 
-  return (
-    <View style={[styles.container, { paddingTop: top, height }]}>
-      <View style={styles.contentContainer} onLayout={(e) => {
-        setPlayerHeight(e.nativeEvent.layout.height);
-      }}>
-        <IconButton
-          style={styles.backButton}
-          iconName={"chevron-back"}
-          iconColor={"#000"}
-          onPress={() => navigation.pop()}
-        />
-        <Text style={styles.title}>{mix.title}</Text>
-        <Text style={styles.artist}>{mix.team}</Text>
-        {Platform.OS !== "web" && (
-          <>
-            <ClipsViewer
-              currentTrack={currentTrack}
-              selectedMode={selectedMode}
-              playing={playing}
-              playTimer={playTimer}
-              mixId={mixId}
-              pause={pause}
-              skipTo={skipTo}
-            />
-            <MediaControls
-              playing={playing}
-              play={play}
-              pause={pause}
-              skipForward={skipForward}
-              skipBackward={skipBackward}
-            />
-          </>
-        )}
-      </View>
-      <DraggableTracks
-        navigation={navigation}
-        mixId={mixId}
-        mix={mix}
-        playerHeight={playerHeight}
-        reorder={reorderQueue}
-        currentTrack={currentTrack}
-        selectedMode={selectedMode}
-        selectTrack={selectTrack}
-        deselectTrack={deselectTrack}
-      />
-      <IconButton
-        style={{
-          ...styles.addButton, bottom: bottom + 10, left: (width - 50) / 2
-        }}
-        iconName={"add"}
-        iconColor={"#fff"}
-        iconStyle={styles.addIcon}
-      />
-    </View>
-  );
+	return (
+		<View style={[styles.container, { paddingTop: top, height }]}>
+			<View
+				style={styles.contentContainer}
+				onLayout={(e) => {
+					setPlayerHeight(e.nativeEvent.layout.height);
+				}}
+			>
+				<IconButton
+					style={styles.backButton}
+					iconName={"chevron-back"}
+					iconColor={"#758A8D"}
+					onPress={() => navigation.pop()}
+				/>
+				<Text style={styles.title}>{mix.title}</Text>
+				<Text style={styles.artist}>{mix.team}</Text>
+				{Platform.OS !== "web" && (
+					<>
+						<ClipsViewer
+							currentTrack={currentTrack}
+							selectedMode={selectedMode}
+							playing={playing}
+							playTimer={playTimer}
+							mixId={mixId}
+							pause={pause}
+							skipTo={skipTo}
+						/>
+						<MediaControls
+							playing={playing}
+							play={play}
+							pause={pause}
+							skipForward={skipForward}
+							skipBackward={skipBackward}
+						/>
+					</>
+				)}
+			</View>
+			<DraggableTracks
+				navigation={navigation}
+				mixId={mixId}
+				mix={mix}
+				playerHeight={playerHeight}
+				reorder={reorderQueue}
+				currentTrack={currentTrack}
+				selectedMode={selectedMode}
+				selectTrack={selectTrack}
+				deselectTrack={deselectTrack}
+			/>
+			<IconButton
+				style={{
+					...styles.addButton,
+					bottom: bottom + 10,
+					left: (width - 50) / 2,
+				}}
+				iconName={"add"}
+				iconColor={"#fff"}
+				iconStyle={styles.addIcon}
+			/>
+		</View>
+	);
 };
 
 export default MixScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "space-between"
-  },
-  contentContainer: {
-    marginBottom: 20
-  },
-  backButton: {
-    margin: 10,
-    alignSelf: "flex-start"
-  },
-  title: {
-    fontSize: FontSize.HEADER,
-    textAlign: "center"
-  },
-  artist: {
-    fontSize: FontSize.SUBHEADER,
-    fontStyle: "italic",
-    textAlign: "center"
-  },
-  blackBox: {
-    backgroundColor: "#000",
-    padding: 10,
-    marginTop: 20,
-    height: 200,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  blackBoxText: {
-    color: "#fff"
-  },
-  addButton: {
-    position: "absolute",
-    backgroundColor: "#000",
-    width: 50,
-    height: 50,
-    borderRadius: 10000
-  },
-  addIcon: {
-    marginLeft: 3
-  }
+	container: {
+		flex: 1,
+		justifyContent: "space-between",
+		backgroundColor: "transparent",
+		backgroundImage: "linear-gradient(to bottom, #292929, #0D252D)",
+	},
+	contentContainer: {
+		marginBottom: 20,
+	},
+	backButton: {
+		margin: 10,
+		alignSelf: "flex-start",
+		color: "#758A8D",
+	},
+	title: {
+		fontSize: FontSize.HEADER,
+		textAlign: "center",
+		color: "#fff",
+	},
+	artist: {
+		fontSize: FontSize.SUBHEADER,
+		fontStyle: "italic",
+		textAlign: "center",
+		color: "#C6C6C6",
+	},
+	blackBox: {
+		backgroundColor: "#000",
+		padding: 10,
+		marginTop: 20,
+		height: 200,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	blackBoxText: {
+		color: "#fff",
+	},
+	addButton: {
+		position: "absolute",
+		backgroundColor: "#000",
+		width: 50,
+		height: 50,
+		borderRadius: 10000,
+	},
+	addIcon: {
+		marginLeft: 3,
+	},
 });
